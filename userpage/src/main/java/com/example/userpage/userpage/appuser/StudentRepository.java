01dfdd01e053672317ce1e4815d941d0493d41f3
roulpriyambada09 <roulpriyambada@gmail.com>
2021-11-11 00:13:03 +0530

    created registration and request page

--- userpage/src/main/java/com/example/userpage/userpage/appuser/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.userpage.userpage.appuser;
-
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(readOnly = true)
-public interface StudentRepository {
-    Optional<AppUser> findByEmail(String email);
-}
---
+++ userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.example.userpage.userpage.registration;
+
+public class RegistrationService {
+
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
