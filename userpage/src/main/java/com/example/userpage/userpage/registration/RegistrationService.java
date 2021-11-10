4f98cba0098e9ef3f3611cf16accd28dbe0c50f9
roulpriyambada09 <roulpriyambada@gmail.com>
2021-11-11 00:28:40 +0530

    implemented websecurity config

--- userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationService.java
+++ userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationService.java
@@ -3,6 +3,6 @@ package com.example.userpage.userpage.registration;
 public class RegistrationService {
 
     public String register(RegistrationRequest request) {
-        return null;
+        return "works";
     }
 }
---
+++ userpage/src/main/java/com/example/userpage/userpage/security/PasswordEncoder.java
@@ -0,0 +1,4 @@
+package com.example.userpage.userpage.security;
+
+public class PasswordEncoder {
+}
