158b91a34743eb529f411489d49e5af064a36798
roulpriyambada09 <roulpriyambada@gmail.com>
2021-11-11 09:56:43 +0530

    password encoder added

--- userpage/src/main/java/com/example/userpage/userpage/appuser/AppUserService.java
+++ userpage/src/main/java/com/example/userpage/userpage/appuser/AppUserService.java
@@ -1,13 +1,29 @@
 package com.example.userpage.userpage.appuser;
 
+
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
+
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
--- userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationService.java
+++ userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationService.java
@@ -1,5 +1,9 @@
 package com.example.userpage.userpage.registration;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
 
     public String register(RegistrationRequest request) {
