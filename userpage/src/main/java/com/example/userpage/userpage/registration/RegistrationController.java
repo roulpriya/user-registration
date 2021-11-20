61866d1069d907b7afe012f283560a01f6997023
roulpriyambada09 <roulpriyambada@gmail.com>
2021-11-20 23:49:12 +0530

    .

--- userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationController.java
+++ userpage/src/main/java/com/example/userpage/userpage/registration/RegistrationController.java
@@ -1,21 +1,17 @@
 package com.example.userpage.userpage.registration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.security.saml2.Saml2RelyingPartyProperties;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
 
-    private RegistrationService registrationService;
+    private final RegistrationService registrationService;
 
     @PostMapping
-    public String register(@RequestBody RegistrationRequest request){
+    public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 }
